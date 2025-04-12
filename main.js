6a757d918fc110378d2ecf9abb9ca26e1b2d7e95
Youssef <ym688361@gmail.com>
2025-04-12 17:40:16 +0200

    project Speech Recognition

--- main.js
+++ main.js
@@ -2,6 +2,5 @@ let speech = new SpeechSynthesisUtterance();
 /*new SpeechSynthesisUtterance() => بتحول الكلام ل صوت   */
 document.querySelector("button").addEventListener("click", () => { /* لما حد يدوس عليك اعمل كذا */
   speech.text = document.querySelector("textarea").value; /* الكلام اللي مكتوب ف الصندوق  */
-  speech.lang = "ar-EG"; /* اللغة اللي هيتقال بيها */
   window.speechSynthesis.speak(speech); /* الكلام اللي هيتقال */
 });
